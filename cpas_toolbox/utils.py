7f009035f84ce8ebcd1001410a47897786f34f68
Leonard Bruns <leonardb@kth.se>
2022-02-21 12:09:06 +0100

    Add function to resolve paths

--- cpas_toolbox/utils.py
+++ cpas_toolbox/utils.py
@@ -1,8 +1,8 @@
 """This module provides miscellaneous utility functions."""
-from typing import Any
-
 import inspect
+import os
 from pydoc import locate
+from typing import Any, List, Optional
 
 
 def str_to_object(name: str) -> Any:
@@ -14,6 +14,7 @@ def str_to_object(name: str) -> Any:
 
     Args:
         name: Name of the object to resolve.
+
     Returns:
         The object which the provided name refers to. None if no object was found.
     """
@@ -29,3 +30,47 @@ def str_to_object(name: str) -> Any:
 
     # check environment
     return locate(name)
+
+
+def resolve_path(path: str, search_paths: Optional[List[str]] = None) -> str:
+    """Resolves a path to a full absolute path based on search_paths.
+
+    This function considers paths of 5 different cases
+        /... -> absolute path, nothing todo
+        ~/... -> home dir, expand user
+        ./... -> relative to current directory
+        ../... -> relative to current parent directory
+        ... -> relative to search paths
+
+    Current directory is not implicitly included in search paths and has to be added
+    with "." if desired. Search paths are handled in first to last order and considered
+    correct if file or directory exists.
+
+    Returns original path, if file does not exist.
+
+    Args:
+        path: The path to resolve.
+        search_paths:
+            List of search paths to prepend relative paths which are not explicitly
+            relative to current directory.
+            If None, no search paths are assumed.
+    """
+    if search_paths is None:
+        search_paths = []
+
+    if os.path.isabs(path):
+        return path
+
+    parts = path.split(os.sep)
+
+    if parts[0] in [".", ".."]:
+        return os.path.abspath(path)
+    elif parts[0] == "~":
+        return os.path.expanduser(path)
+
+    for search_path in search_paths:
+        resolved_path = os.path.join(search_path, path)
+        if os.path.exists(resolved_path):
+            return os.path.abspath(resolved_path)
+
+    return path
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,9 @@
 """Tests for cpas_toolbox.utils module."""
 import inspect
+import os
+import pathlib
+
+import pytest
 
 from cpas_toolbox import utils
 
@@ -16,3 +20,49 @@ def test_str_to_object() -> None:
 
     assert utils.str_to_object("____") is None
 
+
+def test_resolve_path(tmp_path: pathlib.Path, monkeypatch: pytest.MonkeyPatch) -> None:
+    """Test resolve_path function."""
+    # abs path should not be modified
+    assert utils.resolve_path("/test/abs_path") == "/test/abs_path"
+
+    # create test files
+    (tmp_path / "sub1").mkdir()
+    (tmp_path / "sub1" / "test.file").write_text("")
+    (tmp_path / "sub2").mkdir()
+    (tmp_path / "sub2" / "test.file").write_text("")
+
+    # find directory in search path
+    assert utils.resolve_path("sub1", [tmp_path]) == os.path.abspath(tmp_path / "sub1")
+
+    # file not found in search path should return original path
+    assert utils.resolve_path("test.file", [tmp_path]) == "test.file"
+
+    # file found in second search path
+    assert utils.resolve_path(
+        "test.file", [tmp_path, tmp_path / "sub2"]
+    ) == os.path.abspath(tmp_path / "sub2" / "test.file")
+
+    # file found in first search path
+    assert utils.resolve_path(
+        "test.file", [tmp_path / "sub1", tmp_path / "sub2"]
+    ) == os.path.abspath(tmp_path / "sub1" / "test.file")
+
+    # expand home dir
+    def mock_expanduser(path: str) -> str:
+        return path.replace("~", "/home/name")
+
+    monkeypatch.setattr(os.path, "expanduser", mock_expanduser)
+    assert utils.resolve_path("~/test.file") == "/home/name/test.file"
+
+    # abs path for explicit relative paths (i.e., starting with . or ..)
+    def mock_abspath(path: str) -> str:
+        parts = path.split(os.sep)
+        if parts[0] == ".":
+            return path.replace(".", "/home/name", 1)
+        elif parts[0] == "..":
+            return path.replace("..", "/home/name", 1)
+
+    monkeypatch.setattr(os.path, "abspath", mock_abspath)
+    assert utils.resolve_path("./test.file") == "/home/name/test.file"
+    assert utils.resolve_path("../test.file") == "/home/name/test.file"
